f600296717d83d728810020ca227b59e60c63965
Artem <as@attractgroup.com>
2018-01-18 15:00:36 +0200

    forecast component added

---
+++ src/app/Components/Forecast/Forecast.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'forecast',
+    templateUrl: './Forecast.component.html'
+})
+
+export class ForecastComponent implements OnInit {
+
+    ngOnInit() {
+
+    }
+}
